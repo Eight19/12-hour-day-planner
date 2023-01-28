ea1c6ee4e859cc3d456ca6ffe807c75abad8493f
Tiffany Jones <eight19realty@gmail.com>
2023-01-28 14:57:20 -0500

    update time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,80 @@
-const saveButton =document.querySelector("#.saveBtn");
-const textboxEl = document.querySelector("#.description");
-const today = document.querySelector("#currentDay");
-const timeBlock = $(".time-block");
+
+//-------Display current day at top of CALENDAR----------
+var theDay = moment().format("MMM Do YY");
+$('#currentDay').append(theDay);
+
+
+//---Create the variables to save "time" text to local storage-------
+var time1 = $("9").text();
+var time2 = $("10").text();
+var time3 = $("11").text();
+var time4 = $("12").text();
+var time5 = $("13").text();
+var time6 = $("14").text();
+var time7 = $("15").text();
+var time8 = $("16").text();
+var time9 = $("17").text();
+var time10 = $("18").text();
+
+//Current time done with moment.js
+var currentTime = moment().hours();
+
+
+//***FUNCTION: The function to render local storage information on page load
+$(document).ready(function () {
+
+    //-------Load from local storage-------
+    $('#text-1').html(localStorage.getItem('time1'));
+    $('#text-2').html(localStorage.getItem('time2'));
+    $('#text-3').html(localStorage.getItem('time3'));
+    $('#text-4').html(localStorage.getItem('time4'));
+    $('#text-5').html(localStorage.getItem('time5'));
+    $('#text-6').html(localStorage.getItem('time6'));
+    $('#text-7').html(localStorage.getItem('time7'));
+    $('#text-8').html(localStorage.getItem('time8'));
+    $('#text-9').html(localStorage.getItem('time9'));
+    $('#text-10').html(localStorage.getItem('time10'));
 
 
 
-$(".saveBtn").on("click", function() {
-    const timeStamp = $(this).parent(".description").attr("id");
-    const message = $(this).parent().children().eq(1).val();
-    
-    localStorage.setItem(timeStamp, message);
-  }
-  );
+    //****FUNCTION: Save each slot & text to local storage when "Save" button clicked
+    $('button').on("click", function () {
+        localStorage.setItem('time1', $('#text-1').val());
+        localStorage.setItem('time2', $('#text-2').val());
+        localStorage.setItem('time3', $('#text-3').val());
+        localStorage.setItem('time4', $('#text-4').val());
+        localStorage.setItem('time5', $('#text-5').val());
+        localStorage.setItem('time6', $('#text-6').val());
+        localStorage.setItem('time7', $('#text-7').val());
+        localStorage.setItem('time8', $('#text-8').val());
+        localStorage.setItem('time9', $('#text-9').val());
+        localStorage.setItem('time10', $('#text-10').val());
+    });
 
 
 
 
-// Time Blocks //
-if (currentHour < hour9) {
-  $("#7am-text").addClass("future");
-} else if (currentHour > hour9) {
-  $("#7am-text").addClass("past");
-} else if (currentHour = hour9) {
-  $("#7am-text").addClass("present");
+
+   function sectionColor() {
+
+    $(".row time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id"));
+
+        if (currentTime < timeBlock) {
+            $(this).addClass("future").removeClass("past");
+        }
+        else if (currentTime > timeBlock) {
+            $(this).addClass("past").removeClass("present");
+        }
+        else if (currentTime === timeBlock) {
+            $(this).addClass("present").removeClass("future")
+        }
+    });
+
+
 }
+sectionColor()
+});
 
 
 
-// SAVE ANY DATA TO LOCAL STORAGE//
-$("#7am-row .description").val(localStorage.getItem("7am-row"));
-
-$("#8am-row .description").val(localStorage.getItem("#8am-row"));
-
-$("#9am-row .description").val(localStorage.getItem("9am-row"));
-
-$("#10am-row .description").val(localStorage.getItem("#10am-row"));
-
-$("#11am-row .description").val(localStorage.getItem("11am-row"));
-
-$("#12pm-row .description").val(localStorage.getItem("12pm-row"));
-
-$("#1pm-row .description").val(localStorage.getItem("1pm-row"));
-
-$("#2pm-row .description").val(localStorage.getItem("2pm-row"));
-
-$("#3pm-row .description").val(localStorage.getItem("3pm-row"));
-
-$("#4pm-row .description").val(localStorage.getItem("4pm-row"));
-
-$("#5pm-row .description").val(localStorage.getItem("5pm-row"));
-    
-  timeBlock.each(function() 
-  { const calendarHour = parseInt($(this).attr("id"));
-    
-  $(this).children().eq(1).val(localStorage.getItem(calendarHour));
-    
-    if (calendarHour < currentHour) {
-      $(this).addClass("past");
-    } else if (calendarHour == currentHour) {
-      $(this).addClass("present");
-    } else {
-      $(this).addClass("future");
-    }
-  }
-  )
-  
-    today.text(currentDate);
