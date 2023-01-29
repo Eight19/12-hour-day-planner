720a9b62cd1d70381a151402a673182c6dc3d38a
Tiffany Jones <eight19realty@gmail.com>
2023-01-28 21:00:55 -0500

    Added javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,80 +1,44 @@
+//*Day, date, and time imported from day.js*//
+$(function () {
+  today = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+  const todaysDate = $('#todaysDate');
+  todaysDate.text(today);
+  const currentTime = dayjs().hour();
+  console.log(today);
+  
 
-//-------Display current day at top of CALENDAR----------
-var theDay = moment().format("MMM Do YY");
-$('#currentDay').append(theDay);
-
-
-//---Create the variables to save "time" text to local storage-------
-var time1 = $("9").text();
-var time2 = $("10").text();
-var time3 = $("11").text();
-var time4 = $("12").text();
-var time5 = $("13").text();
-var time6 = $("14").text();
-var time7 = $("15").text();
-var time8 = $("16").text();
-var time9 = $("17").text();
-var time10 = $("18").text();
-
-//Current time done with moment.js
-var currentTime = moment().hours();
-
-
-//***FUNCTION: The function to render local storage information on page load
-$(document).ready(function () {
-
-    //-------Load from local storage-------
-    $('#text-1').html(localStorage.getItem('time1'));
-    $('#text-2').html(localStorage.getItem('time2'));
-    $('#text-3').html(localStorage.getItem('time3'));
-    $('#text-4').html(localStorage.getItem('time4'));
-    $('#text-5').html(localStorage.getItem('time5'));
-    $('#text-6').html(localStorage.getItem('time6'));
-    $('#text-7').html(localStorage.getItem('time7'));
-    $('#text-8').html(localStorage.getItem('time8'));
-    $('#text-9').html(localStorage.getItem('time9'));
-    $('#text-10').html(localStorage.getItem('time10'));
-
-
-
-    //****FUNCTION: Save each slot & text to local storage when "Save" button clicked
-    $('button').on("click", function () {
-        localStorage.setItem('time1', $('#text-1').val());
-        localStorage.setItem('time2', $('#text-2').val());
-        localStorage.setItem('time3', $('#text-3').val());
-        localStorage.setItem('time4', $('#text-4').val());
-        localStorage.setItem('time5', $('#text-5').val());
-        localStorage.setItem('time6', $('#text-6').val());
-        localStorage.setItem('time7', $('#text-7').val());
-        localStorage.setItem('time8', $('#text-8').val());
-        localStorage.setItem('time9', $('#text-9').val());
-        localStorage.setItem('time10', $('#text-10').val());
+  //*Saves user input to local storage*//
+  $('.time-block').on('click', '.saveBtn', function(event) {
+    
+   const value = $(this).prev().val();
+   const hour = $(this).parent().attr('id');
+   localStorage.setItem(hour, value);
+    
+  })
+//*Retrieve saved user input from local storage on page reload*//
+  $('.description').each(function() {
+   const userInput = localStorage.getItem($(this).parent().attr('id'));
+    $(this).val(userInput);
+    console.log(userInput);
+  })
+//*Log's time according to time blocks*.//
+  $('.description').each(function() {
+   const timeSlot = parseInt($(this).parent(0).attr('id'));
+    console.log(timeSlot);
+      if (timeSlot < currentTime) {
+        $(this).parent().addClass('past');
+      } else if (timeSlot === currentTime) {
+        $(this).parent().addClass('present');
+      } else {
+        $(this).parent().addClass('future');
+      }
+    
     });
-
-
-
-
-
-   function sectionColor() {
-
-    $(".row time-block").each(function () {
-        var timeBlock = parseInt($(this).attr("id"));
-
-        if (currentTime < timeBlock) {
-            $(this).addClass("future").removeClass("past");
-        }
-        else if (currentTime > timeBlock) {
-            $(this).addClass("past").removeClass("present");
-        }
-        else if (currentTime === timeBlock) {
-            $(this).addClass("present").removeClass("future")
-        }
-    });
-
-
-}
-sectionColor()
-});
-
-
-
+   
+  });
+  //*Clear's planner contents and local storage*//
+  $("#clearSectionBtn").click(function (event) {
+    event.preventDefault;
+  $("textarea").val(""); localStorage.clear();
+  console.log(clearSection);
+  });
